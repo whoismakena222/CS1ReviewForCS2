48894e1e42ce6711023e54649d22b6245b0afe02
whoismakena222 <makena_ndiritu25@milton.edu>
2024-09-08 13:10:51 -0400

    sep 8 2024

--- src/CoffeeDrink.java
+++ src/CoffeeDrink.java
@@ -1,17 +1,21 @@
 // CoffeeDrink object class
 
-class CoffeeDrink {
+public class CoffeeDrink {
 
     int numberOfCups;
     String flavor;
     boolean hasWhippedCream;
 
     // Constructor for CoffeeDrink class
-    public CoffeeDrink() {
+    public CoffeeDrink(int paramnumberOfCups, String paramflavor, boolean paramhasWhippedCream) {
         // TODO
         // Edit this constructor to accept three parameters,
         // one for each CoffeeDrink variable.
         // Assign those parameter values to the class-wide variables.
+        numberOfCups = paramnumberOfCups;
+        flavor = paramflavor;
+        hasWhippedCream = paramhasWhippedCream;
+
     }
 
     // Method to print information about the CoffeeDrink
@@ -19,6 +23,12 @@ class CoffeeDrink {
         // TODO
         // Make this method print order details.
         // Include information stored in each variable.
+        if(hasWhippedCream){
+            System.out.println("You ordered " + numberOfCups + " cups of " + flavor + " coffee with whipped cream." );
+        }
+        else{
+            System.out.println("You ordered " + numberOfCups + " cups of " + flavor + " coffee with no whipped cream.");
+        }
 
     }
 }
--- src/JavaBeansAndBytes.java
+++ src/JavaBeansAndBytes.java
@@ -13,6 +13,16 @@ public class JavaBeansAndBytes {
     boolean isOpen;
     int yearFounded;
 
+    String flavor;
+
+    int randomInt;
+
+    double randomNum;
+
+    CoffeeDrink orderOne;
+
+    CoffeeDrink orderTwo;
+
     public static void main(String[] args) {
         // Create a new CoffeeShop instance to run everything.
         JavaBeansAndBytes cafe = new JavaBeansAndBytes();
@@ -34,21 +44,25 @@ public class JavaBeansAndBytes {
 
         // TODO
         // Update the year founded and print the new yearFounded in a sentence
+        yearFounded = 1990;
+        System.out.println("We were founded in " + yearFounded + ".");
 
         // Calling methods
         randomDiscount();
         // TODO
         // Add parameter to specialOfTheDay (drink of your choice)
-        specialOfTheDay();
+        specialOfTheDay("Iced Strawberry Matcha");
         countCups();
         baristasChoice();
 
         // TODO
         // Create two CoffeeDrink objects
-
+        orderOne = new CoffeeDrink(2, "Iced Mocha", true);
+        orderTwo = new CoffeeDrink(1, "Espresso", false);
         // TODO
         // Print out the order details
-
+        orderOne.printInfo();
+        orderTwo.printInfo();
 
     }
 
@@ -57,13 +71,19 @@ public class JavaBeansAndBytes {
         // TODO
         // Make this method generate a random integer discount (0-30%)
         // and print the result.
+        randomInt = (int)(Math.random()*31);
+        System.out.println("You recieved a discount of " + randomInt + "%");
+
     }
 
     // Method with a parameter for the special of the day
-    public void specialOfTheDay() {
+    public void specialOfTheDay(String paramflavor) {
         // TODO
         // Make this method accept a parameter representing the day’s special.
         // Print the day's special.
+        flavor = paramflavor;
+        System.out.println("today's special is" + flavor);
+
     }
 
     // Method to show loops
@@ -74,13 +94,17 @@ public class JavaBeansAndBytes {
         // Write three different for loops that print the indicated numbers:
 
         // Print 1 to 5
-
-
+        for(int x = 1; x < 6; x = x + 1){
+        System.out.println(x);
+        }
         // Print 2, 5, 8, 11
-
-
+        for(int x = 2; x < 12; x = x+3){
+            System.out.println(x);
+        }
         // Print 8 to 0
-
+        for(int x = 8; x >=0; x = x-1){
+            System.out.println(x);
+        }
     }
 
     // Method to recommend a coffee based on a random number
@@ -88,6 +112,20 @@ public class JavaBeansAndBytes {
         // TODO
         // Make this method generate a random decimal between 0 and 1
         // and print one of four drink recommendations based on its value.
+        randomNum = Math.random();
+        if(randomNum < 0.25){
+            System.out.println("We reccomend you try our Hot Cappuccino with macademia nut milk!");
+        }
+        if(randomNum >= 0.25 && randomNum < 0.5){
+            System.out.println("We reccomend you try our Iced vanilla latte with caramel syrup!");
+        }
+        if(randomNum >= 0.5 && randomNum < 0.75){
+            System.out.println("We reccomend you try our creamy Hot chocolate!");
+            }
+        if (randomNum >= 0.75 && randomNum <1){
+            System.out.println("We reccomend you try our fresh Cold Brew!");
+        }
+
     }
 }
 
